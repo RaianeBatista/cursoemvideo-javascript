b47adf93ee60414cc3009304dbf856c92bbec9ff
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-05-29 17:22:34 -0300

    feat: add conditions

---
+++ aula011/exer008.js
@@ -0,0 +1,10 @@
+//console.log('O console funcionou corretamente!')
+var vel = 65;
+
+console.log(`A velocidade do seu carro é ${vel}km/h`);
+//Indentação, recuo, use a tecla tab do teclado
+if (vel > 60) { //Condição simples
+  console.log(`Você ultrpassou a velocidade permitida. MULTADO!`);
+}
+
+console.log(`Dirija sempre usando cinto de segurança`);
---
+++ aula011/exer009.js
@@ -0,0 +1,17 @@
+//Condição composta
+var país = "França";
+fruta = "Morango";
+
+console.log(`Vivendo em ${país}`);
+if (país == "Brasil") {
+  console.log("Você é brasileiro!");
+} else {
+  console.log("Você é estrangeiro!");
+}
+
+console.log(`A fruta é ${fruta}`);
+if (fruta != "Melancia") {
+  console.log("Suco de fruta comun");
+} else {
+  console.log("Suco de melancia");
+}
