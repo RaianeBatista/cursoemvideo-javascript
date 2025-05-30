a20b7419c71918979cbbb6369aecfec225823fed
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-05-30 14:02:49 -0300

    feat: add repetition structures, while, do while and for

---
+++ aula013/ambiente.js
@@ -0,0 +1,52 @@
+// console.log('Tudo bem?')
+var contador = 1;
+
+while (contador <= 6) {
+  while (contador <= 3) {
+    //   //console.log("Tudo bem?");
+    //   console.log(`Passo: ${contador}`);
+    contador++;
+  }
+
+  do {
+    //console.log(`Passo: ${contador}`);
+    contador++;
+  } while (contador <= 3);
+
+  var c = 1;
+
+  do {
+    // console.log(`${c}`);
+    c++;
+  } while (c <= 5);
+  //console.log("Lançar");
+
+  //Estrutura de Repetição while
+  var d = 1;
+
+  while (d <= 5) {
+    // console.log(d);
+    d++;
+  }
+
+  //Estrutura de Repetição do while
+
+  var a = 7;
+
+  do {
+    // console.log(a);
+    a--;
+  } while (a >= 0);
+
+  //Estrutura de Repetição for
+
+  for (b = 1; b <= 5; b++) {
+    // console.log(b);
+  }
+
+  //console.log("Vai Começar");
+  for (cont = 1; cont <= 8; cont++) {
+    // console.log(cont);
+  }
+  //console.log("FIM");
+}
