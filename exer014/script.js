642d7be7f4300b06d479ed3cec8aab8a9b73f174
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-05-29 22:34:33 -0300

    feat: add script.js

--- exer014/script.js
+++ exer014/script.js
@@ -1,21 +1,23 @@
 // alert("Olá");
 
 function carregar() {
-var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var dataAtual = new Date()
-    // var hora = dataAtual.getHours()
-    var hora = 19
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
-        //BOM DIA!
-        img.src = "images/fotomanha.png";
-    } else if (hora >= 12 && hora <= 18) {
-        //BOA TARDE 
-        img.src = "images/fototarde.png";
-    } else {
-        //BOA NOITE
-        img.src = "images/fotonoite.png";
-    }
+  var msg = window.document.getElementById("msg");
+  var img = window.document.getElementById("imagem");
+  var dataAtual = new Date();
+  var hora = dataAtual.getHours();
+   //var hora = 20
+  msg.innerHTML = `Agora são ${hora} horas.`;
+  if (hora >= 0 && hora < 12) {
+    //BOM DIA!
+    img.src = "images/fotomanha.png";
+    document.body.style.background = "#b98106";
+  } else if (hora >= 12 && hora <= 18) {
+    //BOA TARDE
+    img.src = "images/fototarde.png";
+    document.body.style.background = "#628659";
+  } else {
+    //BOA NOITE
+    img.src = "images/fotonoite.png";
+    document.body.style.background = "#010115f7";
+  }
 }
-
