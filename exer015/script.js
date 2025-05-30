ec6ca113535c27a6d27c70d15edcbef6414d6122
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-05-30 10:02:02 -0300

    feat: add script.js

--- exer015/script.js
+++ exer015/script.js
@@ -1,1 +1,7 @@
-alert("Olá");
+function verificar() {
+  var data = new Date();
+  var ano = data.getFullYear();
+    var formAno = document.getElementById("txtano");
+  //  var res = document.getElementById('res')
+    var res = document.querySelector('div#res')
+}
