55ed24dcb810da979fdb1a26e00b98c7dd0ce186
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-06-01 20:44:44 -0300

    feat: add details

--- aula017/objetos.js
+++ aula017/objetos.js
@@ -1,21 +1,26 @@
-// Aula sobre Objetos
+// // Aula sobre Objetos
 
-let num = [5, 8, 4];
+// let num = [5, 8, 4];
 
-// Declaração de objetos
-// Objetos são variaveis que guardam valores (atributos) e funcionalidade(Métodos)
+// // Declaração de objetos
+// // Objetos são variaveis que guardam valores (atributos) e funcionalidade(Métodos)
 
-let amigo = {
-  nome: "José",
-  sexo: "M",
-  peso: 85.4,
-  engordar(p = 0) {
-    console.log("Engordou");
-    this.peso += p;
-  },
-};
+// let amigo = {
+//   nome: "José",
+//   sexo: "M",
+//   peso: 85.4,
+//   engordar(p = 0) {
+//     console.log("Engordou");
+//     this.peso += p;
+//   },
+// };
 
-//console.log(typeof amigo);
-//console.log( amigo);
-amigo.engordar(2);
-console.log(amigo.nome);
+// //console.log(typeof amigo);
+// //console.log( amigo);
+// amigo.engordar(2);
+// console.log(amigo.nome);
+
+let num = 8;
+num **= 2;
+
+console.log(num);
